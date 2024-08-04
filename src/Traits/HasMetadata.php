6bcb7e950bac42feb1934dd38031873b05e89500
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-08-04 18:22:49 +0100

    Trait creation

---
+++ src/Traits/HasMetadata.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AscentCreative\Metadata\Traits;
+
+use AscentCreative\CMS\Traits\Extender;
+
+use Illuminate\Http\Request;
+
+use AscentCreative\Metadata\Models\Metadata;
+
+trait HasMetadata {
+
+    use Extender;
+
+    public function initializeHasMetadata() {
+        $this->addCapturable('metadata');
+    }
+
+    public function saveMetadata($data) {
+
+
+        if (is_null($data)) {
+            return;
+        }
+        // this isn't an ID based sync. 
+        // rather - if a key is not provided here, just leave it alone. Don't delete records
+        // record deletion will occur if value provided is null or blank. Any other value will be stored.
+
+        foreach($data as $key=>$value) {
+
+            if(is_null($value) || $value == '') {
+                $this->metadata()->where('key', $key)->delete();
+            } else {
+                Metadata::updateOrCreate([
+                    'metadatable_type'=>get_class($this),
+                    'metadatable_id'=>$this->id,
+                    'key'=>$key,
+                ], [
+                    'value'=>$value,
+                ]);
+            }
+
+        }
+
+    }
+
+    public function deleteMetadata() {
+        $this->metadata()->delete();
+    }
+
+   
+    public function metadata() {
+        return $this->morphMany(Metadata::class, 'metadatable');
+    }
+   
+
+}
